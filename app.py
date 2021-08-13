1f091445be1218c032aa3b5479498f7f80c4a290
Jonathan Zhang <54372027+JZhang-305@users.noreply.github.com>
2021-08-13 18:22:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import cv2
-import matplotlib.pyplot as plt
 import os
 from keras.models import load_model
 
